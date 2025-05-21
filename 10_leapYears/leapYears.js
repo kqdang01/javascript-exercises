f8704fca246d571845f11b53b015f6ceba6ba270
Khang Dang <113054344+kqdang01@users.noreply.github.com>
2025-05-21 02:50:24 -0500

    completed some exercises

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (!(year % 400))
+    {
+        return true
+    }
+    else if (!(year % 100))
+    {
+        return false;
+    }
+    else if (!(year % 4))
+    {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
